c2e7a1bb45f1e1169ce1cc4b517a4d4708540d74
Marck <76914057+marc1k3y@users.noreply.github.com>
2023-07-10 16:06:13 +0300

    update index.js

--- index.js
+++ index.js
@@ -173,8 +173,8 @@ export function forcedPublication() {
 
 export async function setFilterKeyRC(key) {
   filter_key = key;
-  const query = { link: new RegExp('^' + key) };
-  const right_events = await client.db("events").collection("scheduled").find(query).toArray();
+  const events = await client.db("events").collection("scheduled").find().toArray();
+  const right_events = events.filter((item) => item.link.includes(key));
   serviceLog(`[+] ${filter_key} successful setted, finding ${right_events.length} events`);
 }
 
